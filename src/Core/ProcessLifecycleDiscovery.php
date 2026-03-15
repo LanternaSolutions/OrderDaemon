166a7f3186a978d4b92c579be2e98fd1eb7f7b7c
Yakir Rettig <yakir@lanterntech.io>
2026-03-15 16:10:19 +0100

    Fixed free plugin automated download link, minor fix in processlifecyclediscovery.php

--- src/Core/ProcessLifecycleDiscovery.php
+++ src/Core/ProcessLifecycleDiscovery.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 namespace OrderDaemon\CompletionManager\Core;
 
 // Include functions.php for helper methods
-require_once __DIR__ . '/../../Includes/functions.php';
+require_once __DIR__ . '/../Includes/functions.php';
 
 /**
  * Auto-discovering registry for Process Lifecycle Consolidation.
